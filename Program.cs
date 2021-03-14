a649540c6ee192afb0ee4b137ef399a7ac155558
Hyaku Ju Yon <ichitora114@outlook.com>
2021-03-14 05:15:59 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace _4._2_Mod_Pract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write down your favourite color in English using small letters >");
+
+            var color = Console.ReadLine();
+
+            switch (color)
+            {
+                case "red":
+
+                    Console.BackgroundColor = ConsoleColor.Red;
+
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your fav color is red!");
+
+                    break;
+
+                case "green":
+
+                    Console.BackgroundColor = ConsoleColor.Green;
+
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your fav color is green!");
+
+                    break;
+
+                case "cyan":
+
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your fav color is cyan!");
+
+                    break;
+
+                default:
+
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+
+                    Console.ForegroundColor = ConsoleColor.Red;
+
+                    Console.WriteLine("You didn't pick up the color!");
+
+                    break;
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
